b66138f7739ca18d220916ef45590be8fb501ce3
sobhantk48 <sobhantk48@gmail.com>
2026-06-29 23:20:05 +0330

    remove fake vpn delays and simplify state handling

--- app/src/main/java/com/v2ray/myvpn/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/v2ray/myvpn/viewmodel/MainViewModel.kt
@@ -2,13 +2,10 @@ package com.v2ray.myvpn.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.v2ray.myvpn.model.VpnState
 import com.v2ray.myvpn.subscription.VlessParser
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
@@ -38,30 +35,22 @@ class MainViewModel : ViewModel() {
     }
 
     private fun connect() {
-        viewModelScope.launch {
 
-            try {
-                val config =
-                    VlessParser.parse(
-                        "vless://11111111-1111-1111-1111-111111111111@example.com:443?security=tls&type=tcp&sni=google.com#Test"
-                    )
+        try {
+            val config =
+                VlessParser.parse(
+                    "vless://11111111-1111-1111-1111-111111111111@example.com:443?security=tls&type=tcp&sni=google.com#Test"
+                )
 
-                Log.d(TAG, "CONFIG = $config")
-            } catch (e: Exception) {
-                Log.e(TAG, "Parser error", e)
-            }
-
-            _vpnState.value = VpnState.CONNECTING
-            delay(1500)
-            _vpnState.value = VpnState.CONNECTED
+            Log.d(TAG, "CONFIG = $config")
+        } catch (e: Exception) {
+            Log.e(TAG, "Parser error", e)
         }
+
+        _vpnState.value = VpnState.CONNECTED
     }
 
     private fun disconnect() {
-        viewModelScope.launch {
-            _vpnState.value = VpnState.DISCONNECTING
-            delay(1000)
-            _vpnState.value = VpnState.DISCONNECTED
-        }
+        _vpnState.value = VpnState.DISCONNECTED
     }
 }
