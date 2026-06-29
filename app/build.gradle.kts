9f128afb52ddc7e1e101497d79e120d9e32b5032
sobhantk48 <sobhantk48@gmail.com>
2026-06-29 23:24:18 +0330

    implement Android VpnService integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,3 +77,44 @@ dependencies {
         "androidx.compose.ui:ui-tooling"
     )
 }
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.16.0")
+
+    implementation(
+        "androidx.activity:activity-compose:1.10.1"
+    )
+
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2025.06.00"
+        )
+    )
+
+    implementation("androidx.compose.ui:ui")
+
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
+
+    implementation(
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
+}
