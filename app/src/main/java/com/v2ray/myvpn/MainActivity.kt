b59cb00781b1e8c30bb1dff7db48ed2347be7fa9
sobhantk48 <sobhantk48@gmail.com>
2026-06-29 22:14:31 +0330

    Add MainActivity and Compose UI

--- app/src/main/java/com/v2ray/myvpn/MainActivity.kt
+++ app/src/main/java/com/v2ray/myvpn/MainActivity.kt
@@ -3,16 +3,49 @@ package com.v2ray.myvpn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import com.v2ray.myvpn.ui.theme.V2raySTKTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            V2raySTKTheme {
-                Text("V2ray STK")
+            AppScreen()
+        }
+    }
+}
+
+@Composable
+fun AppScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                text = "V2ray STK",
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            Spacer(
+                modifier = Modifier.height(24.dp)
+            )
+
+            Button(
+                onClick = { }
+            ) {
+                Text("Connect")
             }
         }
     }
