1733ffc7c0667223f36ecf2a0972558a67ee6e3b
sobhantk48 <sobhantk48@gmail.com>
2026-06-29 21:32:50 +0330

    Add initial Gradle project configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(
+        RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "V2ray STK"
+include(":app")
