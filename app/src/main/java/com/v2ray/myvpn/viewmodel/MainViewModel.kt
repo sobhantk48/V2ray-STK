3e65eba4f6540694a82eec10b0e6ab9e7b1d4bef
sobhantk48 <sobhantk48@gmail.com>
2026-06-29 22:42:00 +0330

    Add VPN state management

--- app/src/main/java/com/v2ray/myvpn/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/v2ray/myvpn/viewmodel/MainViewModel.kt
@@ -1,30 +1,49 @@
 package com.v2ray.myvpn.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.v2ray.myvpn.model.VpnState
-import com.v2ray.myvpn.vpn.VpnManager
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
+    private val _vpnState =
+        MutableStateFlow(VpnState.DISCONNECTED)
+
     val vpnState: StateFlow<VpnState>
-        get() = VpnManager.state
+        get() = _vpnState
 
     fun toggle() {
-        when (vpnState.value) {
-            VpnState.DISCONNECTED -> {
-                VpnManager.connect()
-            }
+        when (_vpnState.value) {
 
-            VpnState.CONNECTING -> {
+            VpnState.DISCONNECTED -> {
+                connect()
             }
 
             VpnState.CONNECTED -> {
-                VpnManager.disconnect()
+                disconnect()
             }
 
-            VpnState.DISCONNECTING -> {
-            }
+            else -> {}
+        }
+    }
+
+    private fun connect() {
+        viewModelScope.launch {
+            _vpnState.value = VpnState.CONNECTING
+            delay(1500)
+            _vpnState.value = VpnState.CONNECTED
+        }
+    }
+
+    private fun disconnect() {
+        viewModelScope.launch {
+            _vpnState.value = VpnState.DISCONNECTING
+            delay(1000)
+            _vpnState.value = VpnState.DISCONNECTED
         }
     }
 }
